8fe1a80a095af2b5b614e2b62b388504705867d5
nickkouk <nickkouk@outlook.com>
2016-10-23 00:34:47 +0300

    Created Database Context and Repository classes The ImageRepository class needs to be changed in order to store images in the cloud. For now it just stores images to database.

---
+++ Assessment/Assessment/Models/ImagesDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Assessment.Models
+{
+    public class ImagesDbContext : DbContext
+    {
+        //for demonstration reasons i left the connection string to the default localDb
+        public ImagesDbContext() : base()
+        {
+        }
+        public DbSet<Image> Images { get; set; }
+    }
+}
---
+++ Assessment/Assessment/Repositories/ImageRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Assessment.Models;
+
+namespace Assessment.Repositories
+{
+    public class ImageRepository : IImagesService
+    {
+        ImagesDbContext context = new ImagesDbContext();
+
+        /// <summary>
+        /// Returns all images 
+        /// </summary>
+        /// <returns></returns>
+        public List<Image> GetImages()
+        {
+            return context.Images.ToList();
+        }
+
+        /// <summary>
+        /// Adds the supplied <paramref name="image"/> to the system and returns the Id.
+        /// Part of the operation is to store the Image in the blob storage.
+        /// *This method will have to change,so it will also take a photo parameter
+        /// </summary>
+        public int AddNewImage(Image image)
+        {
+            context.Images.Add(image);
+            context.SaveChanges();
+            return image.Id;
+        }
+
+        /// <summary>
+        /// Deletes the Image with the supplied <paramref name="id"/> from the system 
+        /// and deletes the file from the blob storage as well.
+        /// </summary>
+        public void DeleteImage(int id)
+        {
+            Image entry = context.Images.Find(id);
+            if (entry != null)
+            {
+                context.Images.Remove(entry);
+                context.SaveChanges();
+            }
+        }
+    }
+
+}
